34a9f326f75a2dc41c9d54981f09d481527df910
Luis Caldas <augusto@caldas.ie>
2026-07-19 12:26:45 +0100

    removed ups auto poweroff

--- configuration.nix
+++ configuration.nix
@@ -4,55 +4,6 @@ let
   # The main hostname of the system, also used for network ID
   hostName = "box";
 
-  # Setup UPS auto poweroff
-  clientScript = pkgs.writeShellScript "client-script" ''
-    case $1 in
-      on-batt)
-        ${pkgs.util-linux}/bin/logger -t upssched-cmd "UPS On Battery state exceeded timer value."
-        ${pkgs.systemd}/bin/shutdown now
-        ;;
-      *)
-        ${pkgs.util-linux}/bin/logger -t upssched-cmd "UPS Unrecognized event: $1"
-        ;;
-    esac
-  '';
-
-  path = "/var/lib/nut";
-  
-  # UPS Scheduler Configuration
-  clientSched = pkgs.writeText "client-schedule" ''
-    CMDSCRIPT ${clientScript}
-
-    PIPEFN ${path}/upssched.pipe
-    LOCKFN ${path}/upssched.lock
-
-    AT ONBATT * START-TIMER on-batt 60
-    AT ONLINE * CANCEL-TIMER on-batt
-  '';
-
-  # Shared Configurations
-  sharedConf = {
-    # User to run
-    RUN_AS_USER = "root";
-    # Binaries
-    SHUTDOWNCMD = "${pkgs.systemd}/bin/shutdown now";
-    # Number of power supplies before shutting down
-    MINSUPPLIES = 1;
-    # Query intervals
-    POLLFREQ = 1;
-    POLLFREQALERT = 1;
-    # Debug
-    # DEBUG_MIN = 9;
-  };
-
-  # Default Notify
-  defaultNotify = "SYSLOG+EXEC";
-
-  # Map Notify Flags
-  mapNotifyFlags = listTypes: notification:
-    map (each: [ each notification ]) listTypes;
-
-
 in
 {
   imports =
@@ -63,39 +14,6 @@ in
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-
-  # UPS client
-  power.ups = {
-
-    enable = true;
-    mode = "netclient";
-    schedulerRules = "${clientSched}";
-
-    # UPS Monitor
-    upsmon = {
-
-      # Connection
-      monitor.main = {
-        system = "apc@router";
-        powerValue = 1;
-        user = "admin";
-        passwordFile = "/home/lakituen/secrets/ups-pass.txt";
-        type = "secondary";
-      };
-
-      # Settings
-      settings = sharedConf // {
-        # Binary Scheduler
-        NOTIFYCMD = "${pkgs.nut}/bin/upssched";
-        # Flags to be notified
-        NOTIFYFLAG = mapNotifyFlags [
-          "ONLINE" "ONBATT"
-        ] defaultNotify;
-      };
-
-    };
-
-  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
